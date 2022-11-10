cffff60b0605a745e19e02e6c481485a944a5675
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-11-10 15:48:48 +0100

    app is wrapped in provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -9,8 +9,8 @@ import { Provider } from "react-redux";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}></Provider>
-    <App />
+    <Provider store={store}> <App /></Provider>
+   
   </React.StrictMode>
 );
 
