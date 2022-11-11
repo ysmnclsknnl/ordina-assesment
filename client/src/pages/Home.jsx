84ef925b96932039b89530f7fe23174ab50137d9
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-11-11 11:44:25 +0100

    prettier fixed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,52 +1,50 @@
-import React from 'react';
-import {useState,useEffect} from "react";
-import { netherlandsFlightURL} from '../contants';
-import { topCountries } from '../utilities/topCountries';
+import React from "react";
+import { useState, useEffect } from "react";
+import { netherlandsFlightURL } from "../contants";
+import { topCountries } from "../utilities/topCountries";
 
 const Home = () => {
-const [flightData,setFlightData]=useState(null);
-const [error,setError]=useState(null);
-const [loading,setLoading]=useState(false);
-
+  const [flightData, setFlightData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-   const fetchData =async (url)=>{
-    setLoading(true);
-        try{   const data= await fetch(url, {
-            headers: {
-              Authorization: "Basic eWFzZW1pbmNhbGlza2FuOkh5ZmNsYXNzMzcu"
-            }
-          });
-            const json= await data.json();
-            setFlightData(json);
-       
-    }
-            catch(error){
-           setError(error)}
-           finally{
-               setLoading(false);
-           }
-   
-    } 
+    const fetchData = async (url) => {
+      setLoading(true);
+      try {
+        const data = await fetch(url, {
+          headers: {
+            Authorization: "Basic eWFzZW1pbmNhbGlza2FuOkh5ZmNsYXNzMzcu",
+          },
+        });
+        const json = await data.json();
+        setFlightData(json);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
     fetchData(netherlandsFlightURL);
-  },
-  
-  
- []);
-
-
+  }, []);
 
   return (
     <div>
-        <p>The Netherlands Flights</p>
-        {flightData && (<div><p>{` Number Of Flights Now :${flightData.states.length}`}</p>
-        <h2>Countries:</h2>
-        <div>{topCountries(flightData.states).map((state,index)=>(<p key={index}>{state}</p>))}</div></div>)}
-
-          
+      <p>The Netherlands Flights</p>
+      {flightData && (
+        <div>
+          <p>{` Number Of Flights Now :${flightData.states.length}`}</p>
+          <h2>Countries:</h2>
+          <div>
+            {topCountries(flightData.states).map((state, index) => (
+              <p key={index}>{state}</p>
+            ))}
+          </div>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
