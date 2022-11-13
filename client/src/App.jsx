7fd2b3deaad8f6d09138110b414ff674181e06af
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-11-13 01:21:36 +0100

    prettier fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <header className="App-header"></header>
 
       <Router>
